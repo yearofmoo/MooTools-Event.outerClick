f774f825d1966fc5776ec8c72b30e477affb2754
Matias Niemelä <matias@yearofmoo.com>
2012-01-15 18:38:52 -0500

    Updated README + Copied over the code

---
+++ outerClick.js
@@ -0,0 +1,30 @@
+(function($,$$){
+  var events;
+  var check = function(e){
+    var target = $(e.target);
+    var parents = target.getParents();
+    events.each(function(item){
+      var element = item.element;
+      if (element != target && !parents.contains(element))
+        item.fn.call(element, e);
+    });
+  };
+  Element.Events.outerClick = {
+    onAdd: function(fn){
+      if(!events) {
+        window.addEvent('click', check);
+        events = [];
+      }
+      events.push({element: this, fn: fn});
+    },
+    onRemove: function(fn){
+      events = events.filter(function(item){
+        return item.element != this || item.fn != fn;
+      }, this);
+      if (!events.length) {
+        window.removeEvent('click', check);
+        events = null;
+      }
+    }
+  };
+})(document.id,$$);
